2e7700db41f579a31fca5677230b020abe32cb67
Swapna Chalil <74065235+SPchalil@users.noreply.github.com>
2021-07-08 00:53:51 -0230

    install auth0

--- client/src/index.js
+++ client/src/index.js
@@ -4,10 +4,20 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import{ Auth0Provider } from '@auth0/auth0-react';
+
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Auth0Provider
+    domain = {domain}
+    clientId = {clientId}
+    redirectUri = {window.location.origin}
+    >
+      <App/>
+    </Auth0Provider> 
   </BrowserRouter>,
   document.getElementById('root')
 );
