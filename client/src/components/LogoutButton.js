93480e0722bdac7df216094050a79e57fc65f87c
Swapna Chalil <74065235+SPchalil@users.noreply.github.com>
2021-07-18 23:59:51 -0230

    -

--- client/src/components/LogoutButton.js
+++ client/src/components/LogoutButton.js
@@ -1,5 +1,5 @@
 // src/components/logout-button.js
-/*
+
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
@@ -21,8 +21,8 @@ const LogoutButton = () => {
 
 export default LogoutButton;
 
-*/
 
+/*
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import '../StickyNotesApp.css';
